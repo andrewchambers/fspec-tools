bba5e688b69dcc2489b4d00fa8f7ca4b12394c25
andrewchambers <andrewchamberss@gmail.com>
2020-05-26 10:49:20 +1200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,5 @@ set -xe
 CC=${CC:-gcc}
 CFLAGS=${CFLAGS:--g -O2}
 LDFLAGS=${LDFLAGS:--larchive}
-peg parse.peg -o parse.inc.c
-
 $CC rawar.c -DOUT_FORMAT_TAR=1 $CFLAGS $LDFLAGS -o rawtar
 $CC rawar.c -DOUT_FORMAT_CPIO=1 $CFLAGS $LDFLAGS -o rawcpio
